ef47c76adc64b8e6865d86f1093d5dc981990565
Fundaris <webmaster@fudaris.com>
2013-04-17 09:12:54 +0200

    Testing and see how it works

--- src/org/bukkit/craftbukkit/utils/Versioning.java
+++ src/org/bukkit/craftbukkit/utils/Versioning.java
@@ -4,15 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.bukkit.Bukkit;
 
-import com.google.common.base.Joiner;
 import com.google.common.collect.Maps;
 
 public class Versioning {
